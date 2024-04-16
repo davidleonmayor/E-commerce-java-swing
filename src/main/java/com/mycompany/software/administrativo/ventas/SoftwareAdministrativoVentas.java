4c82d5e33a817318ee2093b81c2c4266600ffaf9
JDavidLeon <jd.leon@unimayor.edu.co>
2024-04-16 01:16:10 -0500

    second idea UI based in https://www.youtube.com/watch?v=AgJMFhxvA40

---
+++ src/main/java/com/mycompany/software/administrativo/ventas/SoftwareAdministrativoVentas.java
@@ -0,0 +1,9 @@
+package com.mycompany.software.administrativo.ventas;
+
+public class SoftwareAdministrativoVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        new Dashboard().setVisible(true);
+    }
+}
