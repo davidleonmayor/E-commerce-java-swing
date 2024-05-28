99ecdffbf6bd48de7fced3a99af3a4db5514857a
JDavidLeon <jd.leon@unimayor.edu.co>
2024-05-28 12:32:15 -0500

    styles img package

--- src/main/java/com/mycompany/software/administrativo/ventas/views/ContainerProductEspesification.java
+++ src/main/java/com/mycompany/software/administrativo/ventas/views/ContainerProductEspesification.java
@@ -1,29 +1,63 @@
 package com.mycompany.software.administrativo.ventas.views;
 
+import javax.swing.ImageIcon;
+import java.awt.Image;
+import javax.swing.Icon;
+import javax.swing.JLabel;
+
 public class ContainerProductEspesification extends javax.swing.JPanel {
+
     public ContainerProductEspesification(String name, /*Img img, */ float unitValue, int quantity) {
         initComponents();
-        
+
         // Establece los valores dinámicamente
         jLabel1.setText("nombre: " + name);
         jLabel3.setText("Cantidad: " + quantity);
         jLabel4.setText("Valor Unitario: " + unitValue);
-        
+
         // Aquí también podrías establecer la imagen si es necesario y el editor lo permite
         // jLabel2.setIcon(new ImageIcon(img));
+//        this.setLocationRelativeTo(this);
+//        SetImageLabel(jLabelPackageIMGContainer, "src/main/java/images/PersonLoginBlueBackground.png");
+// Agregar un listener para asegurar que el JLabel tenga tamaño antes de ajustar la imagen
+        jLabelPackageIMGContainer.addComponentListener(new java.awt.event.ComponentAdapter() {
+            @Override
+            public void componentResized(java.awt.event.ComponentEvent e) {
+                SetImageLabel(jLabelPackageIMGContainer, "src/main/java/images/package.png", 100, 100);
+            }
+        });
     }
-    
+
+//    private void SetImageLabel(JLabel labelName, String root) {
+//        ImageIcon image = new ImageIcon(root);
+//        Icon icon = new ImageIcon(image.getImage().getScaledInstance(labelName.getWidth(), labelName.getHeight(), Image.SCALE_DEFAULT));
+//        labelName.setIcon(icon);
+//        this.repaint();
+//    }
+    private void SetImageLabel(JLabel labelName, String root, int width, int height) {
+        try {
+            ImageIcon image = new ImageIcon(root);
+            Image scaledImage = image.getImage().getScaledInstance(width, height, Image.SCALE_DEFAULT);
+            Icon icon = new ImageIcon(scaledImage);
+            labelName.setIcon(icon);
+            this.repaint();
+        } catch (Exception e) {
+            // Manejo de excepciones (puedes personalizarlo según tus necesidades)
+            e.printStackTrace();
+        }
+    }
+
     public void setProductName(String name) {
-    jLabel1.setText("nombre: " + name);
-}
+        jLabel1.setText("nombre: " + name);
+    }
 
-public void setQuantity(int quantity) {
-    jLabel3.setText("Cantidad: " + quantity);
-}
+    public void setQuantity(int quantity) {
+        jLabel3.setText("Cantidad: " + quantity);
+    }
 
-public void setUnitValue(float unitValue) {
-    jLabel4.setText("Valor Unitario: " + unitValue);
-}
+    public void setUnitValue(float unitValue) {
+        jLabel4.setText("Valor Unitario: " + unitValue);
+    }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
@@ -31,37 +65,24 @@ public void setUnitValue(float unitValue) {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
-        jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
+        jLabelPackageIMGContainer = new javax.swing.JLabel();
 
+        setBackground(new java.awt.Color(255, 255, 255));
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel1.setText("nombre: Manzana");
 
-        jPanel2.setBackground(new java.awt.Color(242, 22, 242));
-
-        jLabel2.setText("IMG");
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(34, 34, 34)
-                .addComponent(jLabel2)
-                .addContainerGap(59, Short.MAX_VALUE))
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(39, 39, 39)
-                .addComponent(jLabel2)
-                .addContainerGap(64, Short.MAX_VALUE))
-        );
-
+        jLabel3.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel3.setText("Cantidad: 5");
 
+        jLabel4.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
         jLabel4.setText("Valor Unitario: 1500");
+
+        jLabelPackageIMGContainer.setText(".");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -71,13 +92,14 @@ public void setUnitValue(float unitValue) {
                 .addGap(28, 28, 28)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(86, 86, 86)
+                        .addGap(29, 29, 29)
+                        .addComponent(jLabelPackageIMGContainer)
+                        .addGap(135, 135, 135)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLabel3)
                             .addComponent(jLabel4)))
                     .addComponent(jLabel1))
-                .addContainerGap(175, Short.MAX_VALUE))
+                .addContainerGap(177, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -85,13 +107,15 @@ public void setUnitValue(float unitValue) {
                 .addGap(15, 15, 15)
                 .addComponent(jLabel1)
                 .addGap(36, 36, 36)
+                .addComponent(jLabel3)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel3)
                         .addGap(18, 18, 18)
-                        .addComponent(jLabel4)))
-                .addContainerGap(75, Short.MAX_VALUE))
+                        .addComponent(jLabel4))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(7, 7, 7)
+                        .addComponent(jLabelPackageIMGContainer)))
+                .addContainerGap(132, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -109,10 +133,9 @@ public void setUnitValue(float unitValue) {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabelPackageIMGContainer;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
 }
